b2b6b612806016872fd8d3054670fd25ec1a584c
namnguyen168168 <hoangnam12696@gmail.com>
2022-10-12 18:26:22 +0700

    Update assignment1.sql

--- assignment1.sql
+++ assignment1.sql
@@ -1,23 +1,26 @@
-create table Students(
-	Name varchar(255),
-	Code varchar(20) primary key,
-	Birthday date
-);
 create table Classrooms(
 	Name varchar(255),
 	Code varchar(20) primary key,
-	Room int
-);
-create table Grades(
-	Grade int,
-	Result varchar(20),
-	ID int primary key
+	Room varchar(20)
 );
 create table Subjects(
 	Name varchar(255),
-	ID int primary key
+	ID varchar(20) primary key
+);
+create table Students(
+	Name varchar(255),
+	Code varchar(20) primary key,
+	Birthday date,
+	ClassCode varchar(20) foreign key references Classrooms(Code)
+);
+create table Results(
+	Mark int,
+	ID int primary key,
+	Result varchar(50),
+	StudentCode varchar(20) foreign key references Students(Code),
+	SubjectID varchar(20) foreign key references Subjects(ID)
 );
 create table StudentSubjects(
 	StudentCode varchar(20) foreign key references Students(Code),
-	SubjectID int foreign key references Subjects(ID)
+	SubjectID varchar(20) foreign key references Subjects(ID)
 );
